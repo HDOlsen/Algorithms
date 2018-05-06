19ffcc14ed79ad14bc07ce04d5c5ff6c28b14034
HDOlsen <38845610+HDOlsen@users.noreply.github.com>
2018-05-06 14:31:13 -0500

    Add files via upload

---
+++ large.py
@@ -0,0 +1,3 @@
+words = ["traffic" , "Constantinople" , "me" , "zigzagwanderer" , "heaven" , "jurisprudence"]
+words2 = sorted(words, key=len)
+print (f"The longest word in the list is: %s." % (words2[-1],))
---
+++ small.py
@@ -0,0 +1,5 @@
+words = ["traffic" , "Constantinople" , "me" , "zigzagwanderer" , "heaven" , "jurisprudence"]
+words2 = sorted(words, key=len)
+print (f"The shortest word in the list is: %s." % (words2[0],))
+
+   
